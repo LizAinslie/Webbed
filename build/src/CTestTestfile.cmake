b26699387a40428cdb93ed4952f278c34d630c2a
Linux User <rail@rail-alpine.attlocal.net>
2020-09-06 16:41:06 -0500

    dont want to commit build folder

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rail/code/webbed/src
-# Build directory: /home/rail/code/webbed/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
